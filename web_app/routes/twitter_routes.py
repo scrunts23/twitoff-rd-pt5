cb5d5fd757097539a49aba04832e1596062ead43
scrunts23 <scrunts23@gmail.com>
2020-06-08 21:19:51 -0500

    2 Final

--- web_app/routes/twitter_routes.py
+++ web_app/routes/twitter_routes.py
@@ -2,8 +2,8 @@
 
 from flask import Blueprint, render_template, jsonify
 from web_app.models import db, User, Tweet, parse_records
-from web_app.services.twitter_service import api as twitter_api_client
-from web_app.services.basilica_service import connection as basilica_api_client
+from web_app.services.twitter_services import api as twitter_api_client
+from web_app.services.basilica_services import connection as basilica_api_client
 
 twitter_routes = Blueprint("twitter_routes", __name__)
 
